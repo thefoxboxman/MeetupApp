8fb4ffc9f8bf9293c3a838be1290323f3421e74b
F John De Costa <john@mayerfleur.com>
2019-05-23 14:41:22 +1000

    added TeeUpItem  Comp

---
+++ src/AppHome/TeeUpItem.svelte
@@ -0,0 +1,13 @@
+<article>
+	<header>
+		<h1>Title</h1>
+		<h2>Subtitle</h2>
+	</header>
+	<div class="content">
+<p class=text></p>
+	</div>
+	<footer>
+		<button>Show Details</button>
+			<button>Favourites</button>
+	</footer>
+</article>
