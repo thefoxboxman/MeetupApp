abcd502a67d6a032478e9db42cc7ba35a76b2174
F John De Costa <john@mayerfleur.com>
2019-05-25 10:30:55 +1000

    created badge component

---
+++ src/GlobalComps/Badge.svelte
@@ -0,0 +1,17 @@
+<style>
+span{
+	display: inline-block;
+	margin: 0 0.25rem;
+	border-radius: 3px;
+	border: 1px solid var(--splash-color);
+	background-color: var(--splash-color);
+	color: var(--main-text-color);
+	padding: 0 0.5rem;
+	font-size: 0.8rem;
+}
+</style>
+
+
+<span>
+	<slot/>
+</span>
