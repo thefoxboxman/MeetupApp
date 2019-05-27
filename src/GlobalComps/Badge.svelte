08c956908c22523d1d9285489562eefd2959b360
F John De Costa <john@mayerfleur.com>
2019-05-27 19:41:24 +1000

    Optimized css in Badge

--- src/GlobalComps/Badge.svelte
+++ src/GlobalComps/Badge.svelte
@@ -1,17 +1,10 @@
 <style>
-span{
-	display: inline-block;
-	margin: 0 0.25rem;
-	border-radius: 3px;
-	border: 1px solid var(--splash-color);
-	background-color: var(--splash-color);
-	color: var(--main-text-color);
-	padding: 0 0.5rem;
-	font-size: 0.8rem;
-}
+  span {
+    display: inline-block;
+    margin: 0 0.25rem;
+  }
 </style>
 
-
 <span>
-	<slot/>
+  <slot />
 </span>
