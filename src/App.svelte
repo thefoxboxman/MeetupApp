356b794f6bc97f59c3ae869e2ecb0329abdc23f6
F John De Costa <john@mayerfleur.com>
2019-05-29 11:28:00 +1000

    Move logic for toggles out of App into store

--- src/App.svelte
+++ src/App.svelte
@@ -2,137 +2,100 @@
   import NavHeader from "./GlobalComps/NavHeader.svelte";
   import TeeUpGrid from "./AppHome/TeeUpGrid.svelte";
   import TextInput from "./GlobalComps/TextInput.svelte";
-import Button from "./GlobalComps/Button.svelte";
-	import EditTeeup from "./AppHome/EditTeeup.svelte";
-	import teeups from "./Stores/teeups_store.js";
-	
-  
-  let editMode="null";
+  import Button from "./GlobalComps/Button.svelte";
+  import EditTeeup from "./AppHome/EditTeeup.svelte";
+  import teeups from "./Stores/teeups_store.js";
 
+  let editMode = "null";
 
-  //let teeups = 
+  // let teeups =
 
-function addTeeUp(event) {
-    const newTeeUp = {
-      id: Math.random().toString,
+  function addTeeUp(event) {
+    const newTeeUpData = {
+      //data to be forwarded to store
       date: event.detail.date,
       time: event.detail.time,
       title: event.detail.title,
       description: event.detail.description,
       imageSrc: event.detail.imageSrc,
       altText: event.detail.altText,
-	  venue: event.detail.venue, 
-	};
-	teeups = [newTeeUp, ...teeups];
-	editMode = null;
+      venue: event.detail.venue
+    };
+    // Add new teeup to store by calling addTeeup method and passing newTeeUpData
+    teeups.addTeeup(newTeeUpData);
+    editMode = null;
   }
 
   function togglefavourite(event) {
-	  	//capture id passed up by click listner
-		const id =  event.detail;
-		                
-		// using find(), find the teeup matching the id passed in
-	    const updatedTeeup = {...teeups.find(t=>t.id===id)};
-		
-		//toggle isFavourite value in the specific teeup ie true to false or vise versa
-		updatedTeeup.isFavourite = !updatedTeeup.isFavourite;
-		//find the index of the item that needs to updated
-		const teeupIndex = teeups.findIndex(t => t.id === id);
-		//create new array from original so not to mutate original
-		const updatedTeeups = [...teeups];
-		// in updatedTeeups array replace the specific updatedteeup
-		updatedTeeups[teeupIndex]= updatedTeeup;
-		//finally replace orginal teeups with newly updated teeups array
-		teeups = updatedTeeups;
+    const id = event.detail;     //capture id passed up by click listner
+		teeups.toggleFavourite(id);
   }
 
-function toggleisgoing(event) {
-	  	//capture id passed up by click listner
-		const id =  event.detail;
-		                
-		// using find(), find the teeup matching the id passed in
-	    const updatedTeeup = {...teeups.find(t=>t.id===id)};
-		
-		//toggle isGoing value in the specific teeup ie true to false or vise versa
-		updatedTeeup.isGoing = !updatedTeeup.isGoing;
-		//find the index of the item that needs to updated
-		const teeupIndex = teeups.findIndex(t => t.id === id);
-		//create new array from original so not to mutate original
-		const updatedTeeups = [...teeups];
-		// in updatedTeeups array replace the specific updatedteeup
-		updatedTeeups[teeupIndex]= updatedTeeup;
-		//finally replace orginal teeups with newly updated teeups array
-		teeups = updatedTeeups;
+  function toggleisgoing(event) {
+    const id = event.detail;      //capture id passed up by click listner
+		teeups.toggleIsGoing(id);
   }
 
-function cancelEdit(){
-	editMode = null;
-}
-
+  function cancelEdit() {
+    editMode = null;
+  }
 </script>
 
 <style>
+  /* Grid layout for Appl */
+  #grid-container {
+    display: grid;
+    max-width: 1300px;
+    height: 100vh;
+    grid-template-columns: 1fr 18fr 1fr;
+    grid-template-rows: 50px 1fr;
 
+    grid-template-areas:
+      ". head ."
+      ". main .";
+  }
 
-/* Grid layout for Appl */
-#grid-container{
-	display: grid;
-	max-width:1300px;
-	height: 100vh;
-	grid-template-columns: 1fr 18fr 1fr;
-	grid-template-rows: 50px 1fr;
-	
+  #grid-container > #nav-bar {
+    grid-area: head;
+  }
 
-	grid-template-areas:
-	". head ."
-	". main .";
-}
+  #grid-container > #main-content {
+    grid-area: main;
+  }
 
-#grid-container > #nav-bar{
-	grid-area: head;
-}
-
-#grid-container > #main-content {
-	grid-area: main;
-}
-
-/* Add Teeup Button	*/
-.add-button{
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	margin: 5px 0;
-}
-
-
+  /* Add Teeup Button	*/
+  .add-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 5px 0;
+  }
 </style>
-  
-
 
 <!-- Start App HTML -->
 
 <!-- Grid Container for Whole App -->
-<div id=grid-conatiner>
+<div id="grid-conatiner">
 
-<!-- Navigation Header -->
-<header id="nav-bar">
- <NavHeader /> 
-</header>
+  <!-- Navigation Header -->
+  <header id="nav-bar">
+    <NavHeader />
+  </header>
 
-<!-- Start Main Content -->
-<main id="main-content">
-<!-- Display Add Teeup Button -->
-<div class="add-button">
-<Button  on:click="{()=> editMode ='active'}" >Add New Teeup</Button>
-</div>
+  <!-- Start Main Content -->
+  <main id="main-content">
+    <!-- Display Add Teeup Button -->
+    <div class="add-button">
+      <Button on:click={() => (editMode = 'active')}>Add New Teeup</Button>
+    </div>
 
-  
- {#if editMode === 'active'}
-<EditTeeup on:submit={addTeeUp} on:cancel={cancelEdit}/>
- {/if}
-  <TeeUpGrid {teeups} 
-  on:togglefavourite="{togglefavourite}"
-  on:toggleisgoing="{toggleisgoing}"
-   />
-</main>
+    {#if editMode === 'active'}
+      <EditTeeup on:submit={addTeeUp} on:cancel={cancelEdit} />
+    {/if}
+    <TeeUpGrid
+    
+      teeups={$teeups}
+      on:togglefavourite={togglefavourite}
+      on:toggleisgoing={toggleisgoing} />
+  </main>
 </div>
