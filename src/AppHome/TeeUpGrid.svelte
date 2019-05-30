9d7d741933c7e6c320cd3042592d838bd52c1979
F John De Costa <john@mayerfleur.com>
2019-05-30 19:58:06 +1000

    Added Filtering
    Created teeupfilter component.
    Added fuction to display favourite and isgoing filtering in TeeupGrid

--- src/AppHome/TeeUpGrid.svelte
+++ src/AppHome/TeeUpGrid.svelte
@@ -1,20 +1,49 @@
 // ***** TeeUpGrid - displays teeup items in responsive grid 
 //           each item in grid is displayed by TeeUpItem *****
-// ***** Parent  App      Has Child: TeeUpItem  ******
+// ***** Parent  App       ******
 // ***** Props: teeups;  ******
 // *****  Dispatch: nil;    ******
-// *****    Has Child: TeeUpItem  ******
+// *****    Has Child: TeeUpItem TeeupFilter  ******
 // *****  Passes events: on:showdetails and on:edit from TeeUpItem  ******
 
 
 <script>
   import TeeUpItem from "./TeeUpItem.svelte";
+import TeeupFilter from "./TeeupFilter.svelte"
 
-  export let teeups;
+	export let teeups;
+
+	let seeFavs = false;
+	let seeGoing = false;
+
+	$:filteredTeeups = seeFavs ? teeups.filter(t => t.isFavourite) : seeGoing ? teeups.filter(t => t.isGoing) : teeups;
+
+	function setFilter(event){
+			
+			if(event.detail === 1){
+				seeFavs = true;
+				seeGoing = false;
+				
+			} else if (event.detail === 2) {
+				seeFavs = false;
+				seeGoing = true;
+			
+			} else if (event.detail === 0) {
+				seeFavs = false;
+				seeGoing = false;
+			}
+	}
+
+	
 </script>
 
 <style>
-  section {
+
+.filter_control{
+	margin: 1rem;
+ }
+
+  .teeups {
     margin: 5px;
     width: 100%;
     display: grid;
@@ -23,7 +52,7 @@
   }
 
   @media (min-width: 735px) {
-    section {
+    .teeups {
       margin: 1rem;
       grid-template-columns: repeat(2, 1fr);
     }
@@ -36,7 +65,7 @@
   }
 
   @media (min-width: 1250px) {
-    section {
+    .teeups {
       grid-template-columns: repeat(4, 1fr);
     }
   }
@@ -44,9 +73,11 @@
 
 <!-- Start HTML -->
 <!-- Teeups Listing -->
-
+<div class="filter_control">
+<TeeupFilter on:select={setFilter} />
+</div>
 <section class="teeups">
-  {#each teeups as teeup}
+  {#each filteredTeeups as teeup}
     <TeeUpItem
       id={teeup.id}
       title={teeup.title}
---
+++ src/AppHome/TeeupFilter.svelte
@@ -0,0 +1,69 @@
+// ***** NAME TeeupFilter  to show filtered selection of teeups *****
+// ***** PARENT:        Has Children:  ******
+// ***** PROPS:  ******
+// *****    CHILD:    ******
+
+// *****  DISPATCH:  "all, 0" "favs, 1"  "isgoing, 2" ******
+// *****  PASSES THRU EVENTS: nil  ******
+// *****  STORE:    CUSTOM FUNCTIONS:    *****
+
+<script>
+import { createEventDispatcher } from 'svelte';
+
+
+const dispatch = createEventDispatcher();
+
+function showAll(){
+dispatch("select", 0);
+}
+
+function showFavs(){
+dispatch("select", 1);	
+}
+
+function showGoing(){
+dispatch("select", 2);	
+}
+</script>
+
+<style>
+  div {
+    font-size: 0px;
+  }
+
+  button {
+    background: #aaaaaa;
+    font: inherit;
+    font-size: 1rem;
+    cursor: pointer;
+    border: 1px solid #aaaaaa;
+    padding: 0.5rem 1rem;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  button:first-of-type {
+    border-radius: 5px 0 0 5px;
+  }
+
+  button:last-of-type {
+    border-radius: 0 5px 5px 0;
+  }
+
+  button:hover,
+  button:active,
+  .active {
+    background: #cf0056;
+    border-color: #cf0056;
+    color: white;
+  }
+</style>
+
+<!-- Start HTML -->
+<div>
+<button type=button on:click={showAll}  >All</button>
+<button type=button on:click={showFavs} >Favourites</button>
+<button type=button on:click={showGoing} >Going</button>
+</div>
