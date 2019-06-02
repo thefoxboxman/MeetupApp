8ce6523db992e33f601d85df2a5fb8546b8ec8a9
F John De Costa <john@mayerfleur.com>
2019-06-02 10:22:18 +1000

    Fetching from Firebase

--- src/App.svelte
+++ src/App.svelte
@@ -1,90 +1,96 @@
-//  ***** App - provides functionality to store teeups/meetings.
-//  ***** App provides full CRUD access.
-//  ***** Teeups can be marked favourite and/or going. 
-
-
-
-
 <script>
-  import NavHeader from "./GlobalComps/NavHeader.svelte";
-  import TeeUpGrid from "./AppHome/TeeUpGrid.svelte";
-  import TextInput from "./GlobalComps/TextInput.svelte";
-  import Button from "./GlobalComps/Button.svelte";
-  import EditTeeup from "./AppHome/EditTeeup.svelte";
-	import teeups from "./Stores/teeups_store.js";
-	import TeeupDetail from "./AppHome/TeeupDetail.svelte";
+    //  ***** App - provides functionality to store teeups/meetings.
+    //  ***** App provides full CRUD access.
+    //  ***** Teeups can be marked favourite and/or going.
 
-	let editMode = "null";
-	let page = 'overview';
-	let pageData = {};
-	let editId;
+    import NavHeader from './GlobalComps/NavHeader.svelte'
+    import TeeUpGrid from './AppHome/TeeUpGrid.svelte'
+    import TextInput from './GlobalComps/TextInput.svelte'
+    import Button from './GlobalComps/Button.svelte'
+    import EditTeeup from './AppHome/EditTeeup.svelte'
+    import teeups from './Stores/teeups_store.js'
+    import TeeupDetail from './AppHome/TeeupDetail.svelte'
+    import LoadingSpinner from './GlobalComps/LoadingSpinner.svelte'
 
-fetch ("https://teeupapp-21db3.firebaseio.com/teeups.json")
-.then(res => {
-	if (!res.ok){
-		throw new Error("Fetching fucked up!");
-	}
-	return res.json();
-})
-.then(data => {
-	const loadedTeeups =[];
-	for (const key in data){
-		loadedTeeups.push({
-			...data[key],
-			id: key
-		});
-	}
-	teeups.setTeeups(loadedTeeups);
-})
+    let editMode = 'null'
+    let page = 'overview'
+    let pageData = {}
+    let editId
+    let isLoading = true //test line
 
-  function saveTeeUp(event) {
-		editMode = null;
-		editId = null;
-  }
+    fetch('https://teeupapp-21db3.firebaseio.com/teeups.json')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Fetching fucked up!')
+            }
+            return res.json()
+        })
+        .then(data => {
+            const loadedTeeups = []
+            for (const key in data) {
+                loadedTeeups.push({
+                    ...data[key],
+                    id: key,
+                })
+            }
+            setTimeout(() => {
+                //test line
+                isLoading = false //test line
+                teeups.setTeeups(loadedTeeups) //this line stays
+            }, 1000) //test line
+        })
+        .catch(err => {
+            isLoading = false //test line
+            console.log(err)
+        })
 
-  function cancelEdit() {
-		editMode = null;
-		editId = null;
-	}
-	
-	function showDetails(event) {
-		page = "detailed";
-		pageData.id = event.detail;
-	}
+    function saveTeeUp(event) {
+        editMode = null
+        editId = null
+    }
 
-	function startEdit(event){
-		editMode = 'edit';
-		editId = event.detail;
-	}
+    function cancelEdit() {
+        editMode = null
+        editId = null
+    }
 
-	function closeDetails(){
-		page = "overview";
-		page.id="";
-	}
+    function showDetails(event) {
+        page = 'detailed'
+        pageData.id = event.detail
+    }
+
+    function startEdit(event) {
+        editMode = 'edit'
+        editId = event.detail
+    }
+
+    function closeDetails() {
+        page = 'overview'
+        page.id = ''
+    }
 </script>
 
 <style>
-  /* Grid layout for Appl */
-  #grid-container {
-    display: grid;
-    max-width: 1300px;
-    height: 100vh;
-    grid-template-columns: 1fr 18fr 1fr;
-    grid-template-rows: 50px 1fr;
+    /* Grid layout for Appl */
+    #grid-container {
+        display: grid;
+        max-width: 1300px;
+        height: 100vh;
+        grid-template-columns: 1fr 18fr 1fr;
+        grid-template-rows: 50px 1fr;
 
-    grid-template-areas:
-      ". head ."
-      ". main .";
-  }
+        grid-template-areas:
+            '. head .'
+            '. main .';
+    }
 
-  #grid-container > #nav-bar {
-    grid-area: head;
-  }
+    #grid-container > #nav-bar {
+        grid-area: head;
+    }
 
-  #grid-container > #main-content {
-    grid-area: main;
-  }
-
+    #grid-container > #main-content {
+        grid-area: main;
+    }
 </style>
 
 <!-- Start App HTML -->
@@ -92,31 +98,41 @@ fetch ("https://teeupapp-21db3.firebaseio.com/teeups.json")
 <!-- Grid Container for Whole App -->
 <div id="grid-conatiner">
 
-  <!-- Navigation Header -->
-  <header id="nav-bar">
-    <NavHeader />
-  </header>
+    <!-- Navigation Header -->
+    <header id="nav-bar">
+        <NavHeader />
+    </header>
 
-  <!-- Start Main Content -->
-  <main id="main-content">
-	{#if page === 'overview'}
-
-    {#if editMode === 'edit'}
-      <EditTeeup 
-			id={editId}
-			on:submit={saveTeeUp}
-			 on:cancel={cancelEdit}  />
-    {/if}
-    <TeeUpGrid 
-		teeups={$teeups} 
-		on:showdetails={showDetails}
-		on:edit={startEdit} 
-		/>
-
-		{:else}
-<TeeupDetail id={pageData.id}
-on:edit={editMode= 'edit'}
- on:close={closeDetails}/>
-		{/if}
-  </main>
+    <!-- Start Main Content -->
+    <main id="main-content">
+        {#if page === 'overview'}
+            <!-- enter edit mode -->
+            {#if editMode === 'edit'}
+                <EditTeeup
+                    id={editId}
+                    on:submit={saveTeeUp}
+                    on:cancel={cancelEdit} />
+            {/if}
+            <!--end edit -->
+            <!-- Display Grid -->
+            <!-- if is loading show spinner -->
+            {#if isLoading}
+                <LoadingSpinner />
+            {:else}
+                <!-- display grid -->
+                <TeeUpGrid
+                    teeups={$teeups}
+                    on:showdetails={showDetails}
+                    on:edit={startEdit} />
+            {/if}
+            <!--end show grid -->
+        {:else}
+            <!-- Show Detail of Selected Item -->
+            <TeeupDetail
+                id={pageData.id}
+                on:edit={(editMode = 'edit')}
+                on:close={closeDetails} />
+        {/if}
+        <!--end show detail -->
+    </main>
 </div>
--- src/AppHome/TeeUpGrid.svelte
+++ src/AppHome/TeeUpGrid.svelte
@@ -1,121 +1,125 @@
-// ********************************************
-// ***** TeeUpGrid - displays teeup items in responsive grid // each item in
-grid is displayed by TeeUpItem ***** 
-// ***** Parent App ****** 
-// ***** Props: teeups; ****** 
-// ***** Dispatch: edit; ****** 
-// ***** Has Child: TeeUpItem, TeeupFilter, Button  ****** 
-// ***** Passes events: on:showdetails and on:edit from TeeUpItem, on:edit from TeeupGrid ******
-// ********************************************
-
-
 <script>
-  import { createEventDispatcher } from "svelte";
-  import TeeUpItem from "./TeeUpItem.svelte";
-  import TeeupFilter from "./TeeupFilter.svelte";
-  import Button from "../GlobalComps/Button.svelte";
-import { fly, scale } from 'svelte/transition';
-import { flip } from 'svelte/animate';
+    // ********************************************
+    // ***** TeeUpGrid - displays teeup items in responsive grid // each item in grid is displayed by TeeUpItem *****
+    // ***** Parent App ******
+    // ***** Props: teeups; ******
+    // ***** Dispatch: edit; ******
+    // ***** Has Child: TeeUpItem, TeeupFilter, Button  ******
+    // ***** Passes events: on:showdetails and on:edit from TeeUpItem, on:edit from TeeupGrid ******
+    // ********************************************
 
-  export let teeups;
+    import { createEventDispatcher } from 'svelte'
+    import TeeUpItem from './TeeUpItem.svelte'
+    import TeeupFilter from './TeeupFilter.svelte'
+    import Button from '../GlobalComps/Button.svelte'
+    import { fly, scale } from 'svelte/transition'
+    import { flip } from 'svelte/animate'
 
-  let seeFavs = false;
-  let seeGoing = false;
+    export let teeups
 
-  const dispatch = createEventDispatcher();
+    let seeFavs = false
+    let seeGoing = false
 
-  $: filteredTeeups = seeFavs
-    ? teeups.filter(t => t.isFavourite)
-    : seeGoing
-    ? teeups.filter(t => t.isGoing)
-    : teeups;
+    const dispatch = createEventDispatcher()
 
-  function setFilter(event) {
-    if (event.detail === 1) {
-      seeFavs = true;
-      seeGoing = false;
-    } else if (event.detail === 2) {
-      seeFavs = false;
-      seeGoing = true;
-    } else if (event.detail === 0) {
-      seeFavs = false;
-      seeGoing = false;
+    $: filteredTeeups = seeFavs
+        ? teeups.filter(t => t.isFavourite)
+        : seeGoing
+        ? teeups.filter(t => t.isGoing)
+        : teeups
+
+    function setFilter(event) {
+        if (event.detail === 1) {
+            seeFavs = true
+            seeGoing = false
+        } else if (event.detail === 2) {
+            seeFavs = false
+            seeGoing = true
+        } else if (event.detail === 0) {
+            seeFavs = false
+            seeGoing = false
+        }
     }
-  }
 </script>
 
 <style>
-  .filter_control {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 1rem;
-  }
+    .filter-control {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 1rem;
+    }
 
-  /* Add Teeup Button	*/
-  .add-button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 1rem 0;
-  }
+    /* Add Teeup Button	*/
+    .add-button {
+        /* display: flex;
+        align-items: center;
+        justify-content: space-between; */
+        margin: 1rem 0;
+    }
 
-  .teeups {
-    margin: 5px;
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-gap: 1rem;
-  }
+    .no-meetups {
+        margin: 5rem;
+    }
 
-  @media (min-width: 735px) {
     .teeups {
-      margin: 1rem;
-      grid-template-columns: repeat(2, 1fr);
+        margin: 5px;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 1rem;
     }
-  }
 
-  @media (min-width: 1000px) {
-    section {
-      grid-template-columns: repeat(3, 1fr);
+    @media (min-width: 735px) {
+        .teeups {
+            margin: 1rem;
+            grid-template-columns: repeat(2, 1fr);
+        }
     }
-  }
 
-  @media (min-width: 1250px) {
-    .teeups {
-      grid-template-columns: repeat(4, 1fr);
+    @media (min-width: 1000px) {
+        section {
+            grid-template-columns: repeat(3, 1fr);
+        }
     }
-  }
+
+    @media (min-width: 1250px) {
+        .teeups {
+            grid-template-columns: repeat(4, 1fr);
+        }
+    }
 </style>
-
 
 <!-- Start HTML -->
 <!-- Teeups Listing -->
 <!-- Display Filter Buttons -->
-<div class="filter_control">
-  <TeeupFilter on:select={setFilter} />
-</div>
-<!-- Display Add Teeup Button -->
-<div class="add-button">
-  <Button on:click={() => dispatch('edit')}>Add New Teeup</Button>
-</div>
-
+<section class="filter-control">
+    <!-- Display Add Teeup Button -->
+    <TeeupFilter on:select={setFilter} />
+    <!-- Display Add Teeup Button -->
+    <Button class="add-button" on:click={() => dispatch('edit')}>
+        Add New Teeup
+    </Button>
+</section>
+{#if filteredTeeups.length === 0}
+    <h3 class="no-meetups">No meetups found, you can start adding some.</h3>
+{/if}
 <section class="teeups">
-  {#each filteredTeeups as teeup (teeup.id) }
-    <div transition:scale animate:flip={{duration:400}} >
-		<TeeUpItem
-      id={teeup.id}
-      title={teeup.title}
-      imageSrc={teeup.imageSrc}
-      altText={teeup.altText}
-      description={teeup.description}
-      venue={teeup.venue}
-      date={teeup.date}
-      time={teeup.time}
-      isFav={teeup.isFavourite}
-      isGoing={teeup.isGoing}
-      on:showdetails
-      on:edit />
-			</div>
-  {/each}
+    {#each filteredTeeups as teeup (teeup.id)}
+        <div transition:scale animate:flip={{ duration: 400 }}>
+            <TeeUpItem
+                id={teeup.id}
+                title={teeup.title}
+                imageSrc={teeup.imageSrc}
+                altText={teeup.altText}
+                description={teeup.description}
+                venue={teeup.venue}
+                date={teeup.date}
+                time={teeup.time}
+                isFav={teeup.isFavourite}
+                isGoing={teeup.isGoing}
+                on:showdetails
+                on:edit />
+        </div>
+    {/each}
 </section>
